c95ba3e7bbf5726089900339881d398895161f33
benryves <benryves@benryves.com>
2026-05-29 05:59:03 +0100

    Added the modern PindurTI Debugger from Brass 3

---
+++ PindurTI Debugger/LCD.Designer.cs
@@ -0,0 +1,95 @@
+namespace PindurTI {
+    partial class LCD {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.LCDContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.copyToClipboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.SaveImage = new System.Windows.Forms.SaveFileDialog();
+			this.LCDContext.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// LCDContext
+			// 
+			this.LCDContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToClipboardToolStripMenuItem,
+            this.saveToolStripMenuItem});
+			this.LCDContext.Name = "LCDContext";
+			this.LCDContext.Size = new System.Drawing.Size(172, 48);
+			// 
+			// copyToClipboardToolStripMenuItem
+			// 
+			this.copyToClipboardToolStripMenuItem.Image = global::PindurTI.Properties.Resources.IconCamera;
+			this.copyToClipboardToolStripMenuItem.Name = "copyToClipboardToolStripMenuItem";
+			this.copyToClipboardToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+			this.copyToClipboardToolStripMenuItem.Text = "&Copy to Clipboard";
+			this.copyToClipboardToolStripMenuItem.Click += new System.EventHandler(this.copyToClipboardToolStripMenuItem_Click);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+			this.saveToolStripMenuItem.Text = "&Save...";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// SaveImage
+			// 
+			this.SaveImage.Filter = "Portable Network Graphic (*.png)|*.png|Joint Photographic Experts Group (*.jpg)|*" +
+				".jpg|Windows Bitmap (*.bmp)|*.bmp";
+			// 
+			// LCD
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.ClientSize = new System.Drawing.Size(291, 193);
+			this.ContextMenuStrip = this.LCDContext;
+			this.ControlBox = false;
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.MaximizeBox = false;
+			this.Name = "LCD";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "LCD";
+			this.Load += new System.EventHandler(this.LCD_Load);
+			this.BackgroundImageChanged += new System.EventHandler(this.LCD_BackgroundImageChanged);
+			this.LocationChanged += new System.EventHandler(this.LCD_LocationChanged);
+			this.ResizeEnd += new System.EventHandler(this.LCD_ResizeEnd);
+			this.LCDContext.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.ContextMenuStrip LCDContext;
+        private System.Windows.Forms.ToolStripMenuItem copyToClipboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog SaveImage;
+
+    }
+}
